532573bc4357c10514671fb8cb271995e4f38a17
Oblivia Simplex <oblivia@paranoici.org>
2018-06-13 16:28:31 -0300

    tidying up, and trying to port to python3

--- tool/contracts.py
+++ tool/contracts.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 from web3 import Web3, KeepAliveRPCProvider, IPCProvider
 import os.path
 import json
-import sched, time
+import sched
+import time
 import sys
 import glob
 import sys
@@ -13,19 +14,25 @@ from subprocess import Popen, PIPE, STDOUT
 from values import MyGlobals
 from blockchain import *
 
+
 def compile_contract(filename):
 
-    print('\033[1m[ ] Compiling Solidity contract from the file %s ... \033[0m' % filename, end='')
+    print(
+        '\033[1m[ ] Compiling Solidity contract from the file %s ... \033[0m' %
+        filename,
+        end='')
 
     source_file = filename
-    if (not os.path.isfile(source_file) ):
-        print('\033[91m[-] Contract file %s does NOT exist\033[0m' % source_file )
+    if (not os.path.isfile(source_file)):
+        print('\033[91m[-] Contract file %s does NOT exist\033[0m' %
+              source_file)
         return
 
     with open(filename, 'r') as myfile:
-        code=myfile.read()
-    
-    p=Popen(['solc','--bin','--abi','-o','out',source_file,'--overwrite'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        code = myfile.read()
+
+    p = Popen(['solc', '--bin', '--abi', '-o', 'out', source_file,
+               '--overwrite'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     solo = ''
     while p.poll() is None:
         l = p.stdout.readline()
@@ -41,18 +48,19 @@ def compile_contract(filename):
 
 def get_function_hashes(contract):
 
-        
-    data = json.load(open('out/'+contract+'.abi'))
+    data = json.load(open('out/' + contract + '.abi'))
     fhashes = {}
     for d in data:
         if 'type' in d and d['type'] == 'function':
             hs = ''
-            if 'name' in d: hs = d['name']
+            if 'name' in d:
+                hs = d['name']
             fr = True
             hs += '('
             if 'inputs' in d:
                 for i in d['inputs']:
-                    if not fr: hs +=','
+                    if not fr:
+                        hs += ','
                     hs += i['type']
                     fr = False
             hs += ')'
@@ -63,21 +71,25 @@ def get_function_hashes(contract):
     return fhashes
 
 
-
-def deploy_contract(filename, etherbase, rawcode = False):
+def deploy_contract(filename, etherbase, rawcode=False):
 
     if rawcode:
-        with open(filename, 'r') as myfile: byt=myfile.read()
-        filename_write_address = os.path.basename(filename)+'.address'
-        
+        with open(filename, 'r') as myfile:
+            byt = myfile.read()
+        filename_write_address = os.path.basename(filename) + '.address'
+
     else:
-        filename_abi = os.path.basename(filename)+'.abi'
-        filename_bin = os.path.basename(filename)+'.bin'
-        filename_write_address = os.path.basename(filename)+'.address'
-        with open('./out/'+filename_abi, 'r') as myfile: abi=myfile.read()
-        with open('./out/'+filename_bin, 'r') as myfile: byt=myfile.read()
-        if( len(abi) == 0 or len(byt) == 0 ):
-            print('\033[91m[-] Some of the files is missing or empty: |%s|=%d  |%s|=%d' % (filename_abi, len(abi), filename_bin, len(byt) ) )
+        filename_abi = os.path.basename(filename) + '.abi'
+        filename_bin = os.path.basename(filename) + '.bin'
+        filename_write_address = os.path.basename(filename) + '.address'
+        with open('./out/' + filename_abi, 'r') as myfile:
+            abi = myfile.read()
+        with open('./out/' + filename_bin, 'r') as myfile:
+            byt = myfile.read()
+        if(len(abi) == 0 or len(byt) == 0):
+            print(
+                '\033[91m[-] Some of the files is missing or empty: |%s|=%d  |%s|=%d' %
+                (filename_abi, len(abi), filename_bin, len(byt)))
             print('The contracts have NOT been deployed\033[0m')
             return
 
@@ -86,11 +98,11 @@ def deploy_contract(filename, etherbase, rawcode = False):
     print('\033[1m[ ] Deploying contract \033[0m', end='')
     MyGlobals.web3.personal.unlockAccount(etherbase, '1', 15000)
 
-
     try:
-        transaction_creation_hash = MyGlobals.web3.eth.sendTransaction( {'from':etherbase, 'data': ('0x' if byt[0:2]!='0x' else '') +byt , 'gas':6000000} )
+        transaction_creation_hash = MyGlobals.web3.eth.sendTransaction(
+            {'from': etherbase, 'data': ('0x' if byt[0:2] != '0x' else '') + byt, 'gas': 6000000})
     except Exception as e:
-        print ("Exception: "+str(e))
+        print("Exception: " + str(e))
         return None
 
     global s
@@ -99,11 +111,14 @@ def deploy_contract(filename, etherbase, rawcode = False):
     s.run()
     print('\033[92m confirmed at address: %s \033[0m' % contract_address)
     fullcode = MyGlobals.web3.eth.getCode(contract_address)
-    print('\033[1m[ ] Contract code length on the blockchain : \033[0m \033[92m  %d  : %s... \033[0m' % (len(fullcode), fullcode[:30] ) )
-    with open('./out/'+filename_write_address, 'w') as f:
+    print('\033[1m[ ] Contract code length on the blockchain : \033[0m \033[92m  %d  : %s... \033[0m' % (
+        len(fullcode), fullcode[:30]))
+    with open('./out/' + filename_write_address, 'w') as f:
         f.write(contract_address)
         f.close()
-    print('\033[1m[ ] Contract address saved in file: \033[0m\033[92m%s \033[0m' % ('./out/'+filename_write_address))
+    print(
+        '\033[1m[ ] Contract address saved in file: \033[0m\033[92m%s \033[0m' %
+        ('./out/' + filename_write_address))
 
     return contract_address
 
@@ -112,8 +127,9 @@ def confirm_contract(transaction_creation_hash):
     print('.', end="")
 #    sys.stdout.flush()
     global contract_address
-    receipt = MyGlobals.web3.eth.getTransactionReceipt(transaction_creation_hash)
-    if( receipt is not None):
+    receipt = MyGlobals.web3.eth.getTransactionReceipt(
+        transaction_creation_hash)
+    if(receipt is not None):
         contract_address = receipt['contractAddress']
         return
 
@@ -121,28 +137,34 @@ def confirm_contract(transaction_creation_hash):
 
 
 def rlp_encode(input):
-    if isinstance(input,str):
-        if len(input) == 1 and ord(input) < 0x80: return input
-        else: return encode_length(len(input), 0x80) + input
-    elif isinstance(input,list):
+    if isinstance(input, str):
+        if len(input) == 1 and ord(input) < 0x80:
+            return input
+        else:
+            return encode_length(len(input), 0x80) + input
+    elif isinstance(input, list):
         output = ''
-        for item in input: output += rlp_encode(item)
+        for item in input:
+            output += rlp_encode(item)
         return encode_length(len(output), 0xc0) + output
 
-def encode_length(L,offset):
+
+def encode_length(L, offset):
     if L < 56:
-         return chr(L + offset)
+        return chr(L + offset)
     elif L < 256**8:
-         BL = to_binary(L)
-         return chr(len(BL) + offset + 55) + BL
+        BL = to_binary(L)
+        return chr(len(BL) + offset + 55) + BL
     else:
-         raise Exception("input too long")
+        raise Exception("input too long")
+
 
 def to_binary(x):
     if x == 0:
         return ''
-    else: 
-        return to_binary(int(x / 256)) + chr(x % 256)    
+    else:
+        return to_binary(int(x / 256)) + chr(x % 256)
+
 
 def normalize_address(x, allow_blank=False):
     if allow_blank and x == '':
@@ -157,13 +179,11 @@ def normalize_address(x, allow_blank=False):
     if len(x) != 20:
         raise Exception("Invalid address format: %r" % x)
     return x
-     
+
 
 def predict_contract_address(accountAddress):
 
-    nonce = int(MyGlobals.web3.eth.getTransactionCount(accountAddress)) 
-    adr = Web3.sha3(rlp.encode([normalize_address(accountAddress), nonce]), encoding='bytes')[-40:]
-    return '0x'+adr
-
-
-
+    nonce = int(MyGlobals.web3.eth.getTransactionCount(accountAddress))
+    adr = Web3.sha3(rlp.encode(
+        [normalize_address(accountAddress), nonce]), encoding='bytes')[-40:]
+    return '0x' + adr
